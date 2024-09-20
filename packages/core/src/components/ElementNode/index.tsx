52a4b6ac3c542f178a65d2a4e4e94e7491ccefa2
lvchencheng <1784843081@qq.com>
2024-09-20 15:56:05 +0800

    fix

--- packages/core/src/components/ElementNode/index.tsx
+++ packages/core/src/components/ElementNode/index.tsx
@@ -1,11 +1,12 @@
+import { IEditorElement } from "@/types";
 import { ElementConfig } from "@cgx-designer/utils";
 import { ElFormItem } from "element-plus";
-import { defineComponent, h, inject } from "vue";
+import { defineComponent, h, inject, PropType } from "vue";
 
 const ElementNode = defineComponent({
   props: {
     element: {
-      type: Object,
+      type: Object as PropType<IEditorElement>,
       required: true,
     },
   },
@@ -14,26 +15,26 @@ const ElementNode = defineComponent({
       //先从元素配置那里拿到
       const elementConfig = inject<ElementConfig>("elementConfig");
       //渲染出来的组件
-      const elementRender = elementConfig?.elementRenderMap[props.element!.key];
+      const elementRender = elementConfig?.elementRenderMap[props.element.key];
       return (
         <>
           {/* //!当前选中的元素不是row 就普通元素 然后如果是有默认值 */}
           {/* //!也就是表单元素的 就要加ElFormItem 为了更好的体验而已 */}
-          {Object.keys(props.element!.props).includes("defaultValue") ? (
+          {Object.keys(props.element.props).includes("defaultValue") ? (
             <ElFormItem
               label={
-                !!props.element!.props.label
-                  ? props.element!.props.label
-                  : props.element!.key
+                !!props.element.props.label
+                  ? props.element.props.label
+                  : props.element.key
               }
               class="w-full"
-              labelPosition={props.element!.props.labelPosition}
+              labelPosition={props.element.props.labelPosition}
               style={{ marginBottom: "0px !important" }}
             >
-              {h(elementRender, { elementSchema: props.element! })}
+              {h(elementRender, { elementSchema: props.element })}
             </ElFormItem>
           ) : (
-            <>{h(elementRender, { elementSchema: props.element! })}</>
+            <>{h(elementRender, { elementSchema: props.element })}</>
           )}
         </>
       );
