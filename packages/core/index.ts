84d116d364ed0da54b7f6bb8b7fee0475a5be887
lvchencheng <1784843081@qq.com>
2024-12-02 09:13:18 +0800

    feat:优化elementController

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -10,12 +10,12 @@ import {
 } from "./src/custom";
 import { elementController } from "@cgx-designer/utils";
 
-elementController.registerCustomElement("selectOption", SelectOption);
-elementController.registerCustomElement("colList", ColList);
-elementController.registerCustomElement("ruleSetting", RuleSetting);
-elementController.registerCustomElement("tabList", TabList);
-elementController.registerCustomElement("styleInput", StyleInput);
-elementController.registerCustomElement("idInput", IdInput);
+elementController.registerElementRenderMap("selectOption", SelectOption);
+elementController.registerElementRenderMap("colList", ColList);
+elementController.registerElementRenderMap("ruleSetting", RuleSetting);
+elementController.registerElementRenderMap("tabList", TabList);
+elementController.registerElementRenderMap("styleInput", StyleInput);
+elementController.registerElementRenderMap("idInput", IdInput);
 
 export * from "./src/types/index";
 export * from "./src/components/ElementBuilder/type";
--- packages/utils/core/elementController.tsx
+++ packages/utils/core/elementController.tsx
@@ -2,27 +2,61 @@ import { defineAsyncComponent } from "vue";
 import { template } from "@cgx-designer/ui";
 import { IElementBaseSetting } from "../types/index";
 import { getRandomId } from "../common/index";
-//创建组件基础配置的东西
+import { IEditorElement } from "@cgx-designer/core";
 
+//创建组件基础配置的东西
 export class ElementController {
   //初始的模板，初始的schema
-  elementTemplate: Record<string, any> = {};
-  //能用的是哪些
+  elementTemplate: Record<string, (uuid: Function) => IEditorElement> = {};
+  //能用的是哪些,用于渲染左侧物料列表
   elementList: IElementBaseSetting[] = [];
   //根据key去做能用的是哪些
   elementMap: Record<string, IElementBaseSetting> = {};
   //用于渲染的map
   elementRenderMap: Record<string, any> = {};
+  //注册元素到左侧菜单栏，必须走这里过
   register = (elementBaseConfig: IElementBaseSetting) => {
     if (!elementBaseConfig.noPushList) {
+      //这个方法不抽 因为需要注册到list 必须有下面的方法 只会在这里用到
       this.elementList.push(elementBaseConfig);
     }
-    this.elementTemplate[elementBaseConfig.key] = elementBaseConfig.template;
-    this.elementRenderMap[elementBaseConfig.key] =
-      typeof elementBaseConfig.render === "function"
-        ? defineAsyncComponent(elementBaseConfig.render)
-        : elementBaseConfig.render;
+    this.registerElementTemplate(
+      elementBaseConfig.key,
+      elementBaseConfig.template
+    );
+    this.registerElementRenderMap(
+      elementBaseConfig.key,
+      elementBaseConfig.render
+    );
 
+    this.addElementBaseConfigExtraContent(elementBaseConfig);
+    this.registerElementMap(elementBaseConfig);
+  };
+  //注册元素基础config的Map
+  registerElementMap = (
+    elementBaseConfig: IElementBaseSetting,
+    key?: string
+  ) => {
+    this.elementMap[key ? key : elementBaseConfig.key] = elementBaseConfig;
+  };
+  //注册元素基础schema
+  registerElementTemplate = (
+    key: string,
+    template: (uuid: Function) => IEditorElement
+  ) => {
+    this.elementTemplate[key] = template;
+  };
+  //注册渲染元素列表
+  registerElementRenderMap = (key: string, elementRender: any) => {
+    this.elementRenderMap[key] =
+      typeof elementRender === "function"
+        ? defineAsyncComponent(elementRender)
+        : elementRender;
+  };
+  //追加elementBaseConfig内容
+  private addElementBaseConfigExtraContent = (
+    elementBaseConfig: IElementBaseSetting
+  ) => {
     //每个元素都应该有id输入框 用于复制
     ((elementBaseConfig.config ??= {}).attribute ??= []).unshift({
       label: "组件ID",
@@ -53,17 +87,13 @@ export class ElementController {
         }
       );
     }
-    this.elementMap[elementBaseConfig.key] = elementBaseConfig;
-  };
-  registerCustomElement = (key: string, element: any) => {
-    this.elementRenderMap[key] = element;
   };
 }
 
 //提供一个实例
 export const elementController = new ElementController();
 
-//注册模板
+//注册提供默认的模板
 (function () {
   for (let key in template) {
     elementController.register(template[key]);
