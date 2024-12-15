b3b55676ad0753dce4e86a5b87b7cb65328f1d5a
吕陈程 <lvchencheng@lvchenchengdeMacBook-Pro.local>
2024-12-15 21:51:04 +0800

    feat:决定用写死的方法来写

--- packages/utils/factory/__test__/exportSFC.test.ts
+++ packages/utils/factory/__test__/exportSFC.test.ts
@@ -7,10 +7,6 @@ import * as data from "../data";
 describe("测试创建生成代码", () => {
   it("测试创建formItem代码", () => {
     const builderSchema = testData.builderSchema;
-    const { createFormItem } = createSFCSourceCode(builderSchema);
-
-    //测试用例以button为例子11
-    const formItemSFC = createFormItem(testData.testElementSchema);
-    console.log(formItemSFC);
+    const {} = createSFCSourceCode(builderSchema);
   });
 });
--- packages/utils/factory/exportSFC.ts
+++ packages/utils/factory/exportSFC.ts
@@ -1,27 +1,22 @@
-import { BuilderSchema, IEditorElement } from "@cgx-designer/core";
+import { BuilderSchema, FormSetting, IEditorElement } from "@cgx-designer/core";
 import { defaultCSS } from "./data";
 
 export type ICreateSFCCode = ReturnType<typeof createSFCSourceCode>;
 
-const formItemKeys: string[] = ["label", "size", "labelPosition"];
-
 //创建vue的SFC源码模式
 export const createSFCSourceCode = (builderSchema: BuilderSchema) => {
-  const { elementList, script, formSetting } = builderSchema;
-  const createFormItem = (elementSchema: IEditorElement) => {
-    const props = elementSchema.props;
-    let formItemPropsString = "";
-    Object.keys(props!).forEach((key) => {
-      console.log(key, props![key]);
-      //如果是在formItemKeys中，则添加到form-item的props中
-      if (formItemKeys.includes(key)) {
-        formItemPropsString += ` ${key}="${props![key]}"`;
-      }
-    });
-    console.log(formItemPropsString);
-
-    return `<el-form-item ${formItemPropsString}>
-    </el-form-item>`;
+  const createFormItem = (formSetting: FormSetting) => {
+    return `
+  <el-form
+    ref="${formSetting.refName}"
+    :model="${formSetting.modelName}"
+    :rules="${formSetting.rulesName}"
+    label-width="${formSetting.labelWidth}px"
+    :disabled="${formSetting.disabled}"
+    :size="${formSetting.size}"
+    :label-position="${formSetting.labelPosition}"
+    >
+  </el-form>`;
   };
 
   const createHTML = () => {
@@ -45,7 +40,6 @@ import {
   ElementBuilderExpose,
 } from "cgx-designer";
 import { ref } from "vue";
-import { javaScript } from '../../../play/src/data';
 
 const elementBuilderRef = ref<ElementBuilderExpose | null>(null);
 const builderSchema: BuilderSchema = ${JSON.stringify(builderSchema)};
@@ -56,6 +50,9 @@ const builderSchema: BuilderSchema = ${JSON.stringify(builderSchema)};
   };
 
   const createSFCSourceCode = () => {
+    const { script, elementList, formSetting } = builderSchema;
+    console.log(createFormItem(formSetting));
+
     //总和
     return `
 ${createScriptCode()}
