ca505a58d4d3c4b7c0d8f22b84b0144d2e39ea2f
lvchencheng <1784843081@qq.com>
2024-12-25 09:29:36 +0800

    feat:优化styleIDE组件

--- packages/core/src/custom/StyleIDE/index.tsx
+++ packages/core/src/custom/StyleIDE/index.tsx
@@ -1,6 +1,7 @@
-import { ElButton } from "element-plus";
+import { ElButton, ElMessage, ElTooltip } from "element-plus";
 import IDE from "../../components/IDE/index";
 import { defineComponent, ref, watch } from "vue";
+import { CssIcon } from "@cgx-designer/icons";
 
 const StyleIDE = defineComponent({
   inheritAttrs: false,
@@ -9,8 +10,17 @@ const StyleIDE = defineComponent({
     const styleIDERef = ref<typeof IDE | null>(null);
     const bindValue = ref<string | null>(null);
     const handleSave = () => {
-      const target = JSON.parse(bindValue.value!);
-      emit("update:modelValue", target);
+      //保底机制
+      if (!bindValue.value) {
+        bindValue.value = JSON.stringify({});
+      }
+      try {
+        const target = JSON.parse(bindValue.value!);
+        emit("update:modelValue", target);
+        ElMessage.success("保存成功");
+      } catch (error) {
+        ElMessage.error(`保存失败:${error}`);
+      }
     };
     watch(
       () => attrs.modelValue,
@@ -23,13 +33,24 @@ const StyleIDE = defineComponent({
     );
     return () => (
       <>
-        <div class="font-medium text-sm text-gray-600 flex items-center h-10 dark:text-gray-300 justify-between px-2">
-          <span>CSS样式</span>
+        <div class="flex items-center h-10 justify-between px-2">
+          <div class="flex items-center">
+            <ElTooltip
+              effect="dark"
+              content={`编写的代码必须为JSX的style样式写法\n请在编写完成后进行保存`}
+              placement="top"
+            >
+              <CssIcon class="fill-blue-500 w-5 h-5" />
+            </ElTooltip>
+            <span class="font-medium text-sm text-gray-600 dark:text-gray-300">
+              样式
+            </span>
+          </div>
           <ElButton link type="primary" onClick={handleSave}>
             保存
           </ElButton>
         </div>
-        <div class="w-full h-40 border transition-all duration-300 hover:border-blue-500 dark:border-gray-600 dark:hover:border-blue-500 border-solid rounded-md overflow-hidden">
+        <div class="w-full h-40 border-2 transition-all duration-300 hover:border-blue-500 dark:border-gray-600 dark:hover:border-blue-500 border-solid rounded-md overflow-hidden">
           <IDE
             ref={styleIDERef}
             v-model={bindValue.value}
