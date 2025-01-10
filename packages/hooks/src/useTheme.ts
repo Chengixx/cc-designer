887da237049411d1e2a4069488e63aa188b7f0ed
lcc <1784843081@qq.com>
2025-01-11 00:38:28 +0800

    feat:解决vuetify黑暗模式问题

--- packages/hooks/src/useTheme.ts
+++ packages/hooks/src/useTheme.ts
@@ -1,8 +1,71 @@
-import { onMounted, onUnmounted, ref, watch } from "vue";
+import {
+  DeepReadonly,
+  getCurrentInstance,
+  inject,
+  InjectionKey,
+  onMounted,
+  onUnmounted,
+  Ref,
+  ref,
+  watch,
+} from "vue";
 import { useObserve } from "./useObserve";
 import { useToggle } from "./useToggle";
 
 export type ThemeManage = ReturnType<typeof useTheme>;
+
+export interface Colors extends BaseColors, OnColors {
+  [key: string]: string;
+}
+
+interface BaseColors {
+  background: string;
+  surface: string;
+  primary: string;
+  secondary: string;
+  success: string;
+  warning: string;
+  error: string;
+  info: string;
+}
+
+interface OnColors {
+  "on-background": string;
+  "on-surface": string;
+  "on-primary": string;
+  "on-secondary": string;
+  "on-success": string;
+  "on-warning": string;
+  "on-error": string;
+  "on-info": string;
+}
+
+interface InternalThemeDefinition {
+  dark: boolean;
+  colors: Colors;
+  variables: Record<string, string | number>;
+}
+
+export interface ThemeInstance {
+  readonly isDisabled: boolean;
+  readonly themes: Ref<Record<string, InternalThemeDefinition>>;
+
+  readonly name: Readonly<Ref<string>>;
+  readonly current: DeepReadonly<Ref<InternalThemeDefinition>>;
+  readonly computedThemes: DeepReadonly<
+    Ref<Record<string, InternalThemeDefinition>>
+  >;
+
+  readonly themeClasses: Readonly<Ref<string | undefined>>;
+  readonly styles: Readonly<Ref<string>>;
+
+  readonly global: {
+    readonly name: Ref<string>;
+    readonly current: DeepReadonly<Ref<InternalThemeDefinition>>;
+  };
+}
+
+export const ThemeSymbol: InjectionKey<ThemeInstance> = Symbol.for("vuetify:theme");
 
 export const useTheme = () => {
   const htmlElement: HTMLElement = document.documentElement;
@@ -11,11 +74,17 @@ export const useTheme = () => {
     attributeFilter: ["class"],
   };
   const [isDark, toggleThemeMode] = useToggle(false);
+  const theme = inject(ThemeSymbol, null);
+
   watch(
     () => isDark.value,
     () => {
       htmlElement.classList[isDark.value ? "add" : "remove"]("dark");
-    }
+      if (theme) {
+        theme.global.name.value = isDark.value ? "dark" : "light";
+      }
+    },
+    { immediate: true }
   );
 
   const observerCallback = (mutations: MutationRecord[]) => {
