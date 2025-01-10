e4772672bbb893a9d17802aab857cfabe88db9c2
lvchencheng <1784843081@qq.com>
2025-01-10 14:59:52 +0800

    remove:去除无用组件和指令

--- packages/extensions/src/components/index.ts
+++ packages/extensions/src/components/index.ts
@@ -1,8 +1,7 @@
-import CCButton from "./CCButton";
 import CCard from "./CCard";
 import MoveIcon from "./MoveIcon/index.vue";
 import Divider from "./Divider";
 export * from "./Message/index";
 export * from "./Tabs/index";
 
-export { CCButton, CCard, MoveIcon, Divider };
+export { CCard, MoveIcon, Divider };
--- packages/extensions/src/directives/index.ts
+++ packages/extensions/src/directives/index.ts
@@ -1,2 +1,1 @@
-export * from "./waves";
 export * from "./ripple"
