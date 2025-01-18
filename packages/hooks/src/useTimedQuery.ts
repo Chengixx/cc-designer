64fcbf7861ea6888f89c22e7eeebe4972bee05ad
lcc <1784843081@qq.com>
2025-01-19 00:56:47 +0800

    Chore:优化队列hook

--- packages/hooks/src/useTimedQuery.ts
+++ packages/hooks/src/useTimedQuery.ts
@@ -1,18 +1,46 @@
+import { ref, onMounted, onUnmounted } from "vue";
+
+export interface TimedQueryOptions {
+  timeout?: number;
+  startOnMount?: boolean;
+}
+
 export interface TimedQuery {
   startTimedQuery: () => void;
   stopTimedQuery: () => void;
 }
 
-export const useTimedQuery = (handler: () => void, timeout = 16.66) => {
-  let timer: number;
+export const useTimedQuery = (
+  handler: () => void,
+  options: TimedQueryOptions = { timeout: 16.667, startOnMount: false }
+): TimedQuery => {
+  const { timeout, startOnMount } = options;
+  const timer = ref<number | null>(null);
 
   const startTimedQuery = () => {
     stopTimedQuery();
-    timer = window.setInterval(handler, timeout);
+    timer.value = window.setInterval(handler, timeout);
   };
+
   const stopTimedQuery = () => {
-    window.clearInterval(timer);
+    if (timer.value !== null) {
+      window.clearInterval(timer.value);
+      timer.value = null;
+    }
   };
+
+  if (startOnMount) {
+    onMounted(() => {
+      startTimedQuery();
+    });
+  }
+
+  onUnmounted(() => {
+    if (timer.value !== null) {
+      stopTimedQuery();
+    }
+  });
+
   return {
     startTimedQuery,
     stopTimedQuery,
