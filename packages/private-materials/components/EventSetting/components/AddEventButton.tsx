b3b2e87e7f3ce8782e5b511fb5d21679d9eccff9
展程 <lvchencheng.lcc@alibaba-inc.com>
2025-07-31 13:45:23 +0800

    style:修改事件列表黑暗模式下的问题

--- packages/private-materials/components/EventSetting/components/AddEventButton.tsx
+++ packages/private-materials/components/EventSetting/components/AddEventButton.tsx
@@ -26,13 +26,13 @@ const AddEventButton = defineComponent({
       >
         {{
           tooltip: () => (
-            <div class="c-text-black c-py-1">
+            <div class="c-text-black c-py-1 dark:c-text-white dark:c-bg-darkMode">
               {props.eventList.map((eventItem, index) => {
                 return (
                   <div>
                     {/* 如果不是第一个 可以来一个分割线 */}
                     {index !== 0 && (
-                      <div class="c-h-[1px] c-w-full c-bg-gray-200 c-my-1" />
+                      <div class="c-h-[1px] c-w-full c-bg-gray-200 dark:c-border-darkMode c-my-1" />
                     )}
                     <div class="c-text-sm c-font-medium c-p-2 c-select-none ">
                       {eventItem.title}
@@ -44,7 +44,7 @@ const AddEventButton = defineComponent({
                           onClick={() => {
                             emit("addEvent", event.type);
                           }}
-                          class="c-h-8 c-pl-2 c-flex c-justify-start c-items-center c-cursor-pointer hover:c-bg-gray-200"
+                          class="c-h-8 c-pl-2 c-flex c-justify-start c-items-center c-cursor-pointer hover:c-bg-gray-200 dark:hover:c-bg-gray-800"
                         >
                           {event.describe}
                         </div>
