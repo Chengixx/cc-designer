ddcd04bfd48aad13b681b9025258346ff1e139ad
展程 <lvchencheng.lcc@alibaba-inc.com>
2025-08-21 17:58:56 +0800

    feat:优化initMonacoVue用异步加载

--- packages/element-designer/src/core/useInit.tsx
+++ packages/element-designer/src/core/useInit.tsx
@@ -16,17 +16,18 @@ import { elementController } from "@cgx-designer/controller";
 import { Loading } from "@cgx-designer/extensions";
 import { isMac } from "@cgx-designer/utils";
 
-export const initMonacoVue = async () => {
-  // 注册 Vue 语言支持
-  registerVueLanguage();
+const initMonacoVue = () =>
+  new Promise(() => {
+    // 注册 Vue 语言支持
+    registerVueLanguage();
 
-  // 注册其他语言（Monaco 内置支持）
-  monaco.languages.register({ id: "typescript" });
-  monaco.languages.register({ id: "javascript" });
-  monaco.languages.register({ id: "json" });
-  monaco.languages.register({ id: "css" });
-  monaco.languages.register({ id: "html" });
-};
+    // 注册其他语言（Monaco 内置支持）
+    monaco.languages.register({ id: "typescript" });
+    monaco.languages.register({ id: "javascript" });
+    monaco.languages.register({ id: "json" });
+    monaco.languages.register({ id: "css" });
+    monaco.languages.register({ id: "html" });
+  });
 
 export const initCGXDesigner = (
   manages: {
